07c542e69c366e955c17313222eb90f78a1bd40a
esmerquenn <esmereyvazov@gmail.com>
2024-01-03 21:04:11 +0400

    slider doesnt work so i importagain

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -38,21 +38,36 @@
 // export default Menu;
 import { useState } from "react";
 import Carousel from "react-bootstrap/Carousel";
+// import s1 from "../assets/1.png
+import s1 from "../assets/1.png";
+import s2 from "../assets/2.png";
+import s3 from "../assets/3.png";
+import s4 from "../assets/4.png";
+import s5 from "../assets/5.png";
+import s6 from "../assets/6.png";
+import s7 from "../assets/7.png";
+import s11 from "../assets/11.png";
+import s12 from "../assets/12.png";
+import s13 from "../assets/13.png";
+import s14 from "../assets/14.png";
+import s15 from "../assets/15.png";
+import s16 from "../assets/16.png";
+import s17 from "../assets/17.png";
 function Menu({ flag }) {
   console.log(flag);
   const [index, setIndex] = useState(0);
   const handleSelect = (ind) => {
     setIndex(ind);
   };
-  const arr = [1, 2, 3, 4, 5, 6, 7];
-  const path = "./src/assets/";
+  const arr = [s1, s2, s3, s4, s5, s6, s7];
+  const arr2 = [s11, s12, s13, s14, s15, s16, s17];
 
   return (
     <div className=" menuSlider">
       <Carousel activeIndex={index} onSelect={handleSelect}>
-        {arr.map((item, i) => (
+        {(flag ? arr2 : arr).map((item, i) => (
           <Carousel.Item key={i}>
-            <img src={`${path + (flag ? item + 10 : item)}.png`} alt={item} />
+            <img src={item} alt={item} />
           </Carousel.Item>
         ))}
       </Carousel>
